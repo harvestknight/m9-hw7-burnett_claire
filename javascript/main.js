5be8d3c37f542bfc834d2c0429210acb9fee0c98
Claire Burnett <claireburnett@Claire--Trevor-iMac.local>
2018-10-20 14:31:12 -0600

    Add CSS transition to last two nav links in mobile and desktop

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,52 @@
-var li1 = document.getElementsByTagName('li')[3];
-var li2 = document.getElementsByTagName('li')[4];
-var li3 = document.getElementsByTagName('li')[8];
-var li4 = document.getElementsByTagName('li')[9];
+//change nav background color
+//var li1 = document.getElementsByTagName('li')[3];
+//var li2 = document.getElementsByTagName('li')[4];
+//var li3 = document.getElementsByTagName('li')[8];
+//var li4 = document.getElementsByTagName('li')[9];
 
-console.log(li1, li2, li3, li4);
+//function changeColor () {
+   //li2.classList.add('change-color');   
+//};
+
+
+//li1.addEventListener('mouseenter', changeColor);
+//li2.addEventListener('mouseenter', changeColor);
+//li3.addEventListener('mouseenter', changeColor);
+//li4.addEventListener('mouseenter', changeColor);
+
+//li1.addEventListener('mouseleave', changeColor);
+//li2.addEventListener('mouseleave', changeColor);
+//li3.addEventListener('mouseleave', changeColor);
+//li4.addEventListener('mouseleave', changeColor);
+
+//change section background color
+//var storyBlock = document.getElementsByClassName('col-sm-6 col-md-4');
+
+//console.log(storyBlock);
+
+var link = document.getElementsByTagName('a');
 
 function changeColor () {
-   li2.classList.add('change-color');   
+    this.classList.toggle('change-color');
+};
+
+for (i=0; i < link.length; i++) {
+    link[3].addEventListener('mouseenter', changeColor);
+    link[3].addEventListener('mouseleave', changeColor);
 };
 
 
-li1.addEventListener('mouseenter', changeColor);
-li2.addEventListener('mouseenter', changeColor);
-li3.addEventListener('mouseenter', changeColor);
-li4.addEventListener('mouseenter', changeColor);
+for (i=0; i < link.length; i++) {
+    link[4].addEventListener('mouseenter', changeColor);
+    link[4].addEventListener('mouseleave', changeColor);
+};
 
-li1.addEventListener('mouseleave', changeColor);
-li2.addEventListener('mouseleave', changeColor);
-li3.addEventListener('mouseleave', changeColor);
-li4.addEventListener('mouseleave', changeColor);
+for (i=0; i < link.length; i++) {
+    link[8].addEventListener('mouseenter', changeColor);
+    link[8].addEventListener('mouseleave', changeColor);
+};
+
+for (i=0; i < link.length; i++) {
+    link[9].addEventListener('mouseenter', changeColor);
+    link[9].addEventListener('mouseleave', changeColor);
+};
