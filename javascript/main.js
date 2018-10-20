ec4c32b807e8cd5d0fa3922332df15a83c6532c0
Claire Burnett <claireburnett@Claire--Trevor-iMac.local>
2018-10-20 11:29:43 -0600

    Add mouseenter event listener to both li

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+var li1 = document.getElementsByTagName('li')[3];
+var li2 = document.getElementsByTagName('li')[9];
+
+console.log(li1, li2);
+
+function changeColor () {
+    
+    
+};
+
+
+li1.addEventListener('mouseenter', changeColor);
+li2.addEventListener('mouseenter', changeColor);
